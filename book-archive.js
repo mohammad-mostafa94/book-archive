16a78048470ffac2e781ef9670c6e2340288c4ec
Mohammad Mostafa <mohammad-mostafa94>
2021-09-02 21:45:11 +0600

    image api link added

--- book-archive.js
+++ book-archive.js
@@ -1,23 +1,11 @@
-
-// fetch("https://openlibrary.org/search.json?q=hello")
-// .then(response => response.json())
-// .then(data => console.log(data.docs));
-
-// const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-// fetch(url)
-// .then(response => response.json())
-// .then(data => searchBookResultLength(data.books))
-
-const searchBook = ()=> {
+const searchBook = () => {
     const searchField = document.getElementById("searchId");
     const searchText = searchField.value;
 
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
-    .then(response => response.json())
-    // .then(data => console.log(data));
-    .then(data => searchBookResultLength(data));
-    // .then(data => searchBookResultLength(data.docs));
+        .then(response => response.json())
+        .then(data => searchBookResultLength(data));
 
     searchField.value = '';
     if (searchText === '') {
@@ -29,45 +17,44 @@ const searchBook = ()=> {
     }
 }
 
-// search all results
-const searchBookResultLength = (books) =>{
-    console.log(books.numFound);
+// search all results length start
+const searchBookResultLength = (books) => {
     const bookDocs = books.docs;
-        const element = document.getElementById("length");
-        const div = document.createElement("div");
-            div.innerHTML = `<h1>Total book found ${books.numFound} pieces</h1>`;
-            element.appendChild(div);
-            searchBookResult(bookDocs);
-            
-
-    
+    const element = document.getElementById("length");
+    const div = document.createElement("div");
+    div.innerHTML = `<h1>Total book found ${books.docs.length} pieces</h1>`;
+    element.appendChild(div);
+    searchBookResult(bookDocs);
 }
+// search all results length end
 
-const searchBookResult = findBooks =>{
+// search book result start
+const searchBookResult = findBooks => {
 
     const searchResult = document.getElementById("searchResult");
-    searchResult.textContent= '';
-    console.log(findBooks.length);
-    console.log(findBooks);
-    findBooks.forEach(book => {
-            console.log("book",book);
-            console.log("book - title",book.title);
-            // console.log("book-text-lenght",book.text.length);
-            const div = document.createElement("div");
-            div.classList.add("col");
-            div.innerHTML = `
+    searchResult.textContent = '';
+    const slicedFindBooks = findBooks.slice(0, 12);
+    slicedFindBooks.forEach(book => {
+        const div = document.createElement("div");
+        div.classList.add("col");
+        div.innerHTML = `
+            <img class="h-50" src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="${book.strMealThumb}">
             <h3 class="card-title">Book: ${book.title}</h3>
-            <h5 class="card-title">Author: ${book.author_name ? book.author_name : "not found"}</h5>
-            <p class="card-title">First published: ${book.first_publish_year ? book.first_publish_year : "not found"}</p>
+            <h5 class="card-title">Writer: ${book.author_name ? book.author_name : "not found"}</h5>
+            <p class="card-title">First published: ${book.first_publish_year ? book.first_publish_year :"not found"}</p>
         `
         searchResult.appendChild(div);
-        });
+    });
 }
+// search book result end
 
-{/* <div onclick="mealIds(${book.idMeal})" class="card h-100">
-                <img src="${book.strMealThumb}" class="card-img-top" alt="${book.strMealThumb}">
-            <div class="card-body">
-                    <h5 class="card-title">Book Name: ${book.title}</h5>
-                    <p class="card-text">${book.strInstructions.slice(0,200)}....</p>
-              </div>
-           </div> */}
+
+{
+    /* <div onclick="mealIds(${book.idMeal})" class="card h-100">
+                    <img src="${book.strMealThumb}" class="card-img-top" alt="${book.strMealThumb}">
+                <div class="card-body">
+                        <h5 class="card-title">Book Name: ${book.title}</h5>
+                        <p class="card-text">${book.strInstructions.slice(0,200)}....</p>
+                  </div>
+               </div> */
+}
