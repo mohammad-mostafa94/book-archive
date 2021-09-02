e9694287678d7b28306813634209367c13f843a8
Mohammad Mostafa <mohammad-mostafa94>
2021-09-02 22:27:01 +0600

    almost done

--- book-archive.js
+++ book-archive.js
@@ -8,21 +8,15 @@ const searchBook = () => {
         .then(data => searchBookResultLength(data));
 
     searchField.value = '';
-    if (searchText === '') {
-        const tag = document.createElement("h1");
-        const text = document.createTextNode("Not Book found here");
-        tag.appendChild(text);
-        const element = document.getElementById("notfound");
-        element.appendChild(tag);
-    }
 }
 
 // search all results length start
 const searchBookResultLength = (books) => {
     const bookDocs = books.docs;
     const element = document.getElementById("length");
+    element.textContent = '';
     const div = document.createElement("div");
-    div.innerHTML = `<h1>Total book found ${books.docs.length} pieces</h1>`;
+    div.innerHTML = `<h1>Books are ${books.docs.length === 0? "  not ":books.docs.length} found</h1>`;
     element.appendChild(div);
     searchBookResult(bookDocs);
 }
@@ -38,7 +32,7 @@ const searchBookResult = findBooks => {
         const div = document.createElement("div");
         div.classList.add("col");
         div.innerHTML = `
-            <img class="h-50" src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="${book.strMealThumb}">
+            <img class="h-50" src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="${book.cover_i}">
             <h3 class="card-title">Book: ${book.title}</h3>
             <h5 class="card-title">Writer: ${book.author_name ? book.author_name : "not found"}</h5>
             <p class="card-title">First published: ${book.first_publish_year ? book.first_publish_year :"not found"}</p>
