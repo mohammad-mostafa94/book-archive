baffd19a90b0a727eb13ce79ddd65df0e2b47b71
mohammad-mostafa94 <mohammadmostafa1094@gmail.com>
2021-11-23 18:29:48 +0600

    comment archive

--- book-archive.js
+++ book-archive.js
@@ -16,7 +16,7 @@ const searchBookResultLength = (books) => {
     const element = document.getElementById("length");
     element.textContent = '';
     const div = document.createElement("div");
-    div.innerHTML = `<h1>Books are ${books.docs.length === 0? "  not ":books.docs.length} found</h1>`;
+    div.innerHTML = `<h1>Books are ${books.docs.length === 0 ? "  not " : books.docs.length} found</h1>`;
     element.appendChild(div);
     searchBookResult(bookDocs);
 }
@@ -35,7 +35,7 @@ const searchBookResult = findBooks => {
             <img class="h-50" src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="${book.cover_i}">
             <h3 class="card-title">Book: ${book.title}</h3>
             <h5 class="card-title">Writer: ${book.author_name ? book.author_name : "not found"}</h5>
-            <p class="card-title">First published: ${book.first_publish_year ? book.first_publish_year :"not found"}</p>
+            <p class="card-title">First published: ${book.first_publish_year ? book.first_publish_year : "not found"}</p>
         `
         searchResult.appendChild(div);
     });
